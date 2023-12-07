febb9f1ff4b259e7750464b63cee1408fe5abb4a
Chinmay <chinz.sandur07@gmail.com>
2023-12-07 15:09:41 +0530

    add init timer

--- script.js
+++ script.js
@@ -101,6 +101,20 @@ const wordText = document.querySelector(".word"),
     refreshBtn = document.querySelector(".refresh-word"),
     checkBtn = document.querySelector(".check-word");
 
+let correctWord, timer;
+const initTimer = maxTime => {
+    clearInterval(timer);
+    timer = setInterval(() => {
+        if (maxTime > 0) {
+            maxTime--;
+            return timeText.innerText = maxTime;
+        }
+        alert(`Time off! ${correctWord.toUpperCase()} was the correct word`);
+        initGame();
+    }, 1000);
+}
+
+
 const initGame = () => {
     initTimer(30);
     let randomObj = words[Math.floor(Math.random() * words.length)];
